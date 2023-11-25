3cf17050cf5fd96835d297aae76595e97fea844d
natylr <51606937+natylr@users.noreply.github.com>
2023-11-25 22:29:11 +0200

    update upload image function

--- src/components/story_share/storyEditor.js
+++ src/components/story_share/storyEditor.js
@@ -3,18 +3,18 @@ import ParagraphFrame from './paragraphFrame';
 import '../../styles/storyForm.css';
 
 const StoryEditor = (props) => {
-  const title = props.title 
+  const title = props.title
   const [paragraphsData, setParagraphsData] = useState([]);
-  
+
   const fetchStoryByTitle = async () => {
     try {
       const userId = localStorage.getItem("userId");
       const response = await fetch(`http://localhost:5000/story/get_story?userId=${userId}&title=${title}`);
-  
+
       if (!response.ok) {
         throw new Error(`Failed to fetch story data. Status: ${response.status}`);
       }
-  
+
       const responseBody = await response.text();
       const storyData = JSON.parse(responseBody);
       setParagraphsData(storyData.paragraphs);
@@ -30,35 +30,35 @@ const StoryEditor = (props) => {
   const handleAddParagraph = () => {
     setParagraphsData([...paragraphsData, {}]);
   };
-  
-  const updateImageOfParagraph =(paragraphIndex, newImage)=>{
+
+  const updateImageOfParagraph = (paragraphIndex, newImage) => {
     const updatedParagraphsData = [...paragraphsData];
     updatedParagraphsData[paragraphIndex].paragraphImageData = newImage;
     setParagraphsData(updatedParagraphsData);
   }
-  const updateTextOfParagraph = (paragraphIndex, newText ) =>{
+  const updateTextOfParagraph = (paragraphIndex, newText) => {
     const updatedParagraphsData = [...paragraphsData];
     console.log(paragraphIndex, updatedParagraphsData[paragraphIndex])
-    updatedParagraphsData[paragraphIndex].textData = newText ;
+    updatedParagraphsData[paragraphIndex].textData = newText;
     setParagraphsData(updatedParagraphsData);
   }
-  
+
   const handleSave = async () => {
     try {
       const formData = new FormData();
       formData.append('title', title);
       formData.append('token', localStorage.getItem('token'));
-  
+
       paragraphsData.forEach((paragraph, index) => {
         formData.append(`paragraphs[${index}][textData]`, paragraph.textData);
         formData.append(`paragraphs[${index}][paragraphImageData]`, paragraph.paragraphImageData);
       });
-  
+
       const response = await fetch('http://localhost:5000/story/update_paragraphs', {
         method: 'PUT',
         body: formData,
       });
-  
+
       if (response.ok) {
         const savedStory = await response.json();
         alert('Your changes have been saved');
@@ -80,7 +80,7 @@ const StoryEditor = (props) => {
 
       <div className="paragraphsContainer">
         {paragraphsData.map((paragraphData, index) => (
-          <ParagraphFrame initialText={paragraphData.textData} initialImageUrl={paragraphData.paragraphImageData} index={index} key={index} onUpdataImage={updateImageOfParagraph} onUpdataText={updateTextOfParagraph}/>
+          <ParagraphFrame initialText={paragraphData.textData} initialImageUrl={paragraphData.paragraphImageData} index={index} key={index} onUpdataImage={updateImageOfParagraph} onUpdataText={updateTextOfParagraph} />
         ))}
       </div>
 
