d6427598dc778e8d48eee05cbd7158d158ea13a8
natylr <51606937+natylr@users.noreply.github.com>
2023-12-19 11:56:56 +0200

    update files

--- share-stories-server/secret.js
+++
@@ -1,5 +0,0 @@
-
-const mongoUrl = "mongodb+srv://netanelham:1q2w3e@user-info.hhi99os.mongodb.net/";
-
-const JWT_SECRET = "hvdvay6ert72839289()aiyg8t87qt72393293883uhefiuh78ttq3ifi78272jbkj?[]]pou89ywe";
-module.exports = { mongoUrl, JWT_SECRET };
