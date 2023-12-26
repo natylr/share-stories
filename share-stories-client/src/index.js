f5401a6f432f7bb94057d1d757bc607c6abef04c
natylr <51606937+natylr@users.noreply.github.com>
2023-12-26 20:34:21 +0200

    fix warning in the browser

--- share-stories-client/src/index.js
+++ share-stories-client/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';  // Updated import
 import './styles/index.css';
 import './styles/auth.css';
 import App from './App';
@@ -7,12 +7,15 @@ import reportWebVitals from './reportWebVitals';
 
 const rootElement = document.getElementById('root');
 
-ReactDOM.render(
-  <App />,
-  rootElement
+// Use createRoot from "react-dom/client"
+const root = createRoot(rootElement);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// or send to an analytics
 reportWebVitals();
