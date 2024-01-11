90b6f2bd8875ab832576a9c2e2b880e282e7d111
natylr <51606937+natylr@users.noreply.github.com>
2024-01-11 12:08:40 +0200

    design paragraph frame

--- share-stories-server/routes/storyRoutes.js
+++ share-stories-server/routes/storyRoutes.js
@@ -6,11 +6,10 @@ const upload = multer({ dest: 'uploads/' });
 const StoryController = require("../controllers/StoryController");
 
 router.get("/cards", StoryController.getCards);
-router.post("/add_story",verifyJWT, upload.single('mainImage'), StoryController.addStory);
-router.post("/my_cards",verifyJWT, StoryController.getMyCards);
-router.delete("/delete_story",verifyJWT, StoryController.deleteStoryByTitle);
-router.put("/update_paragraphs",verifyJWT, upload.array('updatedImages'), StoryController.updateParagraphs);
+router.post("/add_story", verifyJWT, upload.single('mainImage'), StoryController.addStory);
+router.post("/my_cards", verifyJWT, StoryController.getMyCards);
+router.delete("/delete_story", verifyJWT, StoryController.deleteStoryByTitle); 
+router.put("/update_paragraphs", verifyJWT, upload.array('updatedImages'), StoryController.updateParagraphs);
 router.get("/get_story", StoryController.getStoryByTitle);
 
 module.exports = router;
-
