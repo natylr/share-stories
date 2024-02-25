0e8ed00df912ba615ebd30202792d4105e6b1f81
natylr <51606937+natylr@users.noreply.github.com>
2024-02-25 23:15:09 +0200

    .

--- share-stories-client/src/config/config.js
+++ share-stories-client/src/config/config.js
@@ -1,2 +1,2 @@
-export const BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://sharestories.store';
-export const defaultAvatarUrl = "http://share-stories.online//default_avatar.png";
+export const BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://sharestories.store';
+export const defaultAvatarUrl = "https://share-stories.online//default_avatar.png";
